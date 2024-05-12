61714f6773573e801f692ea1c19362129e6519af
Govind Prajapat <40013231+Govind08@users.noreply.github.com>
2024-05-12 12:34:34 +0530

    Alerts app

---
+++ AlertApp/alertapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Alerts from '../../../Randomize-colors/randomizecolors/src/Alearts';
+
+function App() {
+  return (
+    <div className="App">
+      <Alerts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Randomize-colors/randomizecolors/src/Alearts.js
@@ -0,0 +1,7 @@
+const Alerts = () => {
+    return ( <div>
+        <h1>Alert</h1>
+    </div> );
+}
+ 
+export default Alerts;
