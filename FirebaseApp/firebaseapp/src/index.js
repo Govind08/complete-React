2c9feb5aea3b3d4637b530cccc8539da56c1677f
Govind Prajapat <40013231+Govind08@users.noreply.github.com>
2024-08-09 07:37:22 +0530

    firebase setup

--- FirebaseApp/firebaseapp/src/index.js
+++ FirebaseApp/firebaseapp/src/index.js
@@ -1,11 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection,getDoc} from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyAxoMSnEc9uwJobofndegX1n0xXZYWlus4",
+  authDomain: "myapp3-6df00.firebaseapp.com",
+  databaseURL: "https://myapp3-6df00.firebaseio.com",
+  projectId: "myapp3-6df00",
+  storageBucket: "myapp3-6df00.appspot.com",
+  messagingSenderId: "666695688115",
+  appId: "1:666695688115:web:f1495996f84dea9634b5c6"
+};
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-console.log("firebase");
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const colRef = collection(db,"Movies")
+
+getDoc(colRef).then(data =>{
+    console.log(data)
+})
